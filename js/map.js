e7124218deda2806d4d1e7f14e1807786d3b77ca
Daniel Ericsson <dericsson@gmail.com>
2024-03-04 17:17:08 +0100

    file structure, placeholders, functional map basics

---
+++ js/map.js
@@ -0,0 +1,67 @@
+// /* Fungerade kod till svg filen. */
+/* Effekten som blir på hover/mouseEntered går att styla här i myStyles */
+
+const myStyles = `
+  filter: drop-shadow(0px 0px 10px rgb(239 239 239 / 1));
+  -moz-transition: all 0.2s ease-in;
+  -o-transition: all 0.2s ease-in;
+  -webkit-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in;
+  fill: rgb(224 46 130 / 0.1);
+  stroke: rgba(224,46,130,0.1);
+  stroke-width: 2;
+`;
+
+const resetStyles = `
+  filter: shadow(0px 0px 0px rgb(0 0 0 / 0));
+  -moz-transition: all 0.2s ease-out;
+  -o-transition: all 0.2s ease-out;
+  -webkit-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+`;
+
+function loadMap() {
+  var map = document.getElementById("map").contentDocument.querySelector("svg");
+  var toolTip = document.getElementById("toolTip");
+
+  // Add event listeners to map element
+  if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // If user agent is not mobile add click listener (for wikidata links)
+  }
+  map.addEventListener("mousemove", mouseEntered, false);
+  map.addEventListener("mouseout", mouseGone, false);
+
+  // Show tooltip on mousemove
+  function mouseEntered(e) {
+    var target = e.target;
+    if (target.classList.contains("hover")) {
+      //targetar cssText som skriver över css i "klumpar" istället för individuella styles - går t.ex. inte att skriva in saker med bindestreck efter "style. "
+      target.style.cssText = myStyles;
+      var details = e.target.attributes;
+
+      // Follow cursor
+      toolTip.style.transform = `translate(${e.offsetX}px, ${e.offsetY}px)`;
+
+      // Tooltip data
+      toolTip.innerHTML = `
+        <ul>
+            <li><b>${details.name.value}</b></li>
+            <li>${details.info.value}</li>
+        </ul>`;
+    }
+  }
+
+  // Clear tooltip on mouseout
+  function mouseGone(e) {
+    var target = e.target;
+    if (target.classList.contains("hover")) {
+      target.style.cssText = resetStyles;
+      toolTip.innerHTML = "";
+    }
+  }
+}
+
+// Calls init function on window load
+window.onload = function () {
+  loadMap();
+};
