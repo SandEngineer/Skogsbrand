0d9dc401fe9a1defdb06ec01511954e515fb95cc
Lerky1337 <linneaerkas@hotmail.com>
2024-03-07 15:56:37 +0100

    knapp + lite annat

--- js/confirmation.js
+++ js/confirmation.js
@@ -9,8 +9,8 @@ const displayDiv = document.getElementById('displayData');
 if (formData) {
   displayDiv.innerHTML = `<p>Datum: ${formData.date}</p><p>Antal biljetter: ${formData.tickets}</p>
                           <p>Förnamn: ${formData.name}</p><p>Efternamn: ${formData.lastname}</p>
-                          <p>Email: ${formData.email}</p><p>Telefonnummer: ${formData.phone}</p>
-                          <p>Betalningsalternativ: ${formData.paymentOption}</p>`;
+                          <p>Bokningsbekräftgelse skickad till: ${formData.email}</p><p>Telefonnummer: ${formData.phone}</p>
+                          <p class="form__label-text">Betalningsalternativ: ${formData.paymentOption}</p>`;
 } else {
   displayDiv.innerHTML = '<p>No data available</p>';
 }
