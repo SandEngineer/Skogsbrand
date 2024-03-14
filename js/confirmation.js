6a4adfc5d910468dcce32311040f61a5019e4d1f
SandEngineer <90037427+SandEngineer@users.noreply.github.com>
2024-03-14 16:59:09 +0100

    stort card färdigt

--- js/confirmation.js
+++ js/confirmation.js
@@ -7,43 +7,35 @@ const formData = JSON.parse(jsonData);
 // Display the data on the page
 const displayDiv = document.getElementById('displayData');
 if (formData) {
-  displayDiv.innerHTML = 
-  `<div class="rubrik row row-cols-1 text-center">
-    <p class="col fs-3">Hej ${formData.name}</p>
-    <p class="brodtext fs-6 col">
-      Tack för din beställning. En orderbekräftelse är skickad till
-      ${formData.email}
-    </p>
-  </div>
-  
-  <div class="d-flex justify-content-center">
-  <p class="rubrik">Bokningsinformation:</p>
-  <div class="d-flex justify-content-center">
-    <ul class="list-group pt-3 fs-6">
-      <li class="list-group-item">
-      <span class="brodtext">Antal biljetter: </span
-      >${formData.tickets}
-      </li>
-      <li class="list-group-item">
-      <span class="brodtext">Förnamn: </span>${formData.name}
-      </li>
-      <li class="list-group-item">
-      <span class="brodtext">Email: </span>${formData.email}
-      </li>
-    </ul>
-    <ul class="list-group pt-3 fs-6">
-      <li class="list-group-item">
-      <span class="brodtext">Datum: </span>${formData.date}
-      </li>
-      <li class="list-group-item">
-        <span class="brodtext">Efternamn: </span>${formData.lastname}
-      </li>
-      <li class="list-group-item">
-        <span class="brodtext">Email: </span>${formData.email}
-      </li>
-    </ul>
-  </div>
-  </div>`;
+  displayDiv.innerHTML = `            <div class="rubrik row row-cols-1 text-center">
+  <p class="col fs-3">Hej ${formData.name}</p>
+  <p class="brodtext fs-6 col">
+    Tack för din beställning. En orderbekräftelse är skickad till
+    ${formData.email}
+  </p>
+</div>
+<ul class="list-group offset-md-1 offset-lg-2 pt-3 fs-6">
+  <li class="list-group-item rubrik">Bokningsinformation:</li>
+  <li class="list-group-item">
+    <span class="brodtext">Antal biljetter: </span
+    >${formData.tickets}
+  </li>
+  <li class="list-group-item">
+    <span class="brodtext">Datum: </span>${formData.date}
+  </li>
+  <li class="list-group-item">
+    <span class="brodtext">Förnamn: </span>${formData.name}
+  </li>
+  <li class="list-group-item">
+    <span class="brodtext">Efternamn: </span>${formData.lastname}
+  </li>
+  <li class="list-group-item">
+    <span class="brodtext">Telefonnummer: </span>${formData.phone}
+  </li>
+  <li class="list-group-item">
+    <span class="brodtext">Email: </span>${formData.email}
+  </li>
+</ul>`;
 } else {
   displayDiv.innerHTML = '<p>No data available</p>';
 }
