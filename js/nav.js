9288914c53d3042514261f68b6c0a8e6a07414e7
Lerky1337 <linneaerkas@hotmail.com>
2024-03-18 13:02:13 +0100

    bild i footer, liten navbar + lista på maps
    liten navbar finns i maps, behöver justeras + klistras in på alla sidor

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,22 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+/* nav hide on scroll */
+var prevScrollpos = window.scrollY || window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY || window.pageYOffset;
+  var navbar = document.getElementById("navbar-sm");
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top =
+      "-100px"; /* Adjust the value to hide the navbar completely */
   }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  prevScrollpos = currentScrollPos;
+};
